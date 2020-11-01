c832059f9fc63f39a0e64900131674f94e66106a
vitaliksokil <vitaliksokil200@gmail.com>
2020-11-01 17:53:14 +0200

    fixed some issue with email verification

--- resources/views/emails/emailVerification.blade.php
+++ resources/views/emails/emailVerification.blade.php
@@ -1,3 +1,5 @@
 <h1>Verify your email</h1>
 <h5>Go to the link below</h5>
-<a href="{{$_SERVER['APP_URL'].'/email/verify/'.$token.'/'.$user_id}}" class="btn btn-primary">VERIFY EMAIL</a>
+{{dd()}}
+{{dd($_SERVER['APP_URL'].'/email/verify/'.$token.'/'.$user_id)}}
+<a href="{{'http://'.parse_url($_SERVER['APP_URL'])['host'].'/email/verify/'.$token.'/'.$user_id}}" class="btn btn-primary">VERIFY EMAIL</a>
