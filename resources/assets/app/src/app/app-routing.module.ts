9e8b5d24cda2603cd7a69746c4b976126850274d
Volodymyr Chykharivskyi <v.chykharivskyi@tenantcloud.com>
2020-12-25 12:07:51 +0200

    Reconstruction of routes

--- resources/assets/app/src/app/app-routing.module.ts
+++ resources/assets/app/src/app/app-routing.module.ts
@@ -1,17 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { SigninComponent } from './auth/pages/signin/signin.component';
-import { SignupComponent } from './auth/pages/signup/signup.component';
-import { ProfileComponent } from './user/pages/profile/profile.component';
-import {ForgotPasswordComponent} from "./auth/pages/forgot-password/forgot-password.component";
-
 const routes: Routes = [
+  { path: 'login', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
-  { path: 'login', component: SigninComponent },
-  { path: 'register', component: SignupComponent },
-  { path: 'profile', component: ProfileComponent },
-  { path: 'forgot-password', component: ForgotPasswordComponent }
 ];
 
 @NgModule({
--- resources/assets/app/src/app/auth/auth.module.ts
+++ resources/assets/app/src/app/auth/auth.module.ts
@@ -4,6 +4,15 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SignupComponent } from "./pages/signup/signup.component";
 import { SigninComponent } from "./pages/signin/signin.component";
 import { ForgotPasswordComponent } from "./pages/forgot-password/forgot-password.component";
+import { RouterModule, Routes } from "@angular/router";
+import { ProfileComponent } from "../user/pages/profile/profile.component";
+
+const authRoutes: Routes = [
+  { path: '', component: SigninComponent },
+  { path: 'register', component: SignupComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'forgot-password', component: ForgotPasswordComponent },
+];
 
 @NgModule({
   declarations: [SignupComponent, SigninComponent, ForgotPasswordComponent],
@@ -11,6 +20,7 @@ import { ForgotPasswordComponent } from "./pages/forgot-password/forgot-password
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
+    RouterModule.forChild(authRoutes),
   ]
 })
 export class AuthModule { }
